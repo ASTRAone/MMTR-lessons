681cd47f7e1ace8c997dd812919a786271a7b56c
ASTRAone <vladwau827@gmail.com>
2020-07-29 00:25:38 +0300

    ipdate accordeon

--- lessons(3)/accordion/js/main.js
+++ lessons(3)/accordion/js/main.js
@@ -1,41 +1,14 @@
-// Открыть первый tab
-let itemTabIndex1 = document.getElementById("one");
+let items = document.querySelectorAll(".container__item");
 
-itemTabIndex1.addEventListener("click", () => {
-    let text = document.getElementById("one").lastElementChild;
-    text.style.display = "block";
+items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        let text = item.querySelector(".container__item-p");
+        
+        items.forEach((item) => {
+            let textBottom = item.querySelector(".container__item-p");
+            textBottom.classList.remove("visible")
+        });
 
-    let textTwo = document.getElementById("two").lastElementChild;
-    textTwo.style.display = "none";
-
-    let textThree = document.getElementById("three").lastElementChild;
-    textThree.style.display = "none";
-});
-
-// Открыть второй tab
-let itemTabIndex2 = document.getElementById("two");
-
-itemTabIndex2.addEventListener("click", () => {
-    let text = document.getElementById("two").lastElementChild;
-    text.style.display = "block";
-
-    let textTwo = document.getElementById("one").lastElementChild;
-    textTwo.style.display = "none";
-
-    let textThree = document.getElementById("three").lastElementChild;
-    textThree.style.display = "none";
-});
-
-// Открыть третий tab
-let itemTabIndex3 = document.getElementById("three");
-
-itemTabIndex3.addEventListener("click", () => {
-    let text = document.getElementById("three").lastElementChild;
-    text.style.display = "block";
-
-    let textTwo = document.getElementById("one").lastElementChild;
-    textTwo.style.display = "none";
-
-    let textThree = document.getElementById("two").lastElementChild;
-    textThree.style.display = "none";
+        text.classList.add("visible")
+    });
 });
