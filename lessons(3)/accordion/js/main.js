f6778a718b2ff49c977a078935aaadec79ddd175
ASTRAone <vladwau827@gmail.com>
2020-07-29 20:40:28 +0300

    update accordeon code

--- lessons(3)/accordion/js/main.js
+++ lessons(3)/accordion/js/main.js
@@ -1,14 +1,21 @@
-let items = document.querySelectorAll(".container__item");
+function Menu(options) {
+    let elem = options.elem;
+  
+    elem.onclick = function(event) {
+        if (event.target.closest('.container__item')) {
+            let items = document.querySelectorAll(".container__item");
 
-items.forEach((item) => {
-    item.addEventListener("click", (e) => {
-        let text = item.querySelector(".container__item-p");
-        
-        items.forEach((item) => {
-            let textBottom = item.querySelector(".container__item-p");
-            textBottom.classList.remove("visible")
-        });
-
-        text.classList.add("visible")
-    });
-});
+            items.forEach((item) => {
+                item.addEventListener("click", (e) => {    
+                    items.forEach((item) => {
+                        let textBottom = item.querySelector(".container__item-p");
+                        textBottom.classList.remove("visible")
+                    });
+                    
+                    let text = item.querySelector(".container__item-p");
+                    text.classList.add("visible")
+                });
+            });
+        }
+    };
+};
