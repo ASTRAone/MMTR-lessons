f0c9e061190daccb18ab4e60809ef30bf2c523aa
ASTRAone <vladwau827@gmail.com>
2020-07-31 22:29:13 +0300

    update code alert modal

--- lessons(3)/alerts/js/main.js
+++ lessons(3)/alerts/js/main.js
@@ -1,29 +1,64 @@
 function Menu(options) {
     let elem = options.elem;
-    
+
     // Открыть модальное окно
     elem.onclick = function(event) {
         if (event.target.closest(".btn")) {
 
-            let cardModal= elem.querySelector(".container-modal");
-            cardModal.style.display = "block";
+            function createElementItem (tag, cls, text) {
+                let teg = document.createElement(tag);
+                teg.className = cls;
+                
+                if (text) {
+                    teg.textContent = text;
+                }
+
+                return teg;
+            };
+
+            let bodyDocument = document.body;
+            let containerModal = createElementItem("div", "container-modal")
+            let ourModalTitle = createElementItem("div", "our-modal__title");
+            let titleModal = createElementItem("h3", "title__modal", "Modal title");
+            let spanModal = createElementItem("span", "span__modal btn-close", `&#10006;`);
+
+            ourModalTitle.appendChild(titleModal);
+            ourModalTitle.appendChild(spanModal);
+            containerModal.appendChild(ourModalTitle);
+
+            let textModal = createElementItem("p", "text__modal", "One fine body");
+            
+            containerModal.appendChild(textModal);
+
+            let ourBtnModal = createElementItem("div", "our-btn__modal");
+            let btnModalOne = createElementItem("button", "btn-modal btn-modal__first btn-close", "Close");
+            let btnModalTwo = createElementItem("button", "btn-modal btn-modal__last btn-save", "Ok");
+
+            ourBtnModal.appendChild(btnModalOne);
+            ourBtnModal.appendChild(btnModalTwo);
+            containerModal.appendChild(ourBtnModal)
+
+            bodyDocument.append(containerModal);
     
             let btn = elem.querySelector(".btn");
             btn.style.display = "none";
-        }
-        // Закрыть модальное окно
-        if (event.target.closest(".btn-close")) {
 
-            let cardModal= elem.querySelector(".container-modal");
-            cardModal.style.display = "none";
+            let btnsClose = document.querySelectorAll(".btn-close");
+            btnsClose.forEach((item) => {
+                item.addEventListener("click", () => {
+                    let btnBody = elem.querySelector(".btn");
+                    btnBody.style.display = "block";
+
+                    let cardModal = document.querySelector(".container-modal");
+                    cardModal.remove()
+                });
+            });
+
+            let btnSave = document.querySelector(".btn-save");
             
-            let btnBody = elem.querySelector(".btn");
-            btnBody.style.display = "block";
-
-        }
-        // Сохранить изменения
-        if (event.target.closest(".btn-save")) {
-            alert("Успешно сохранено")
-        }
+            btnSave.addEventListener("click", () => {
+                alert("Успешно сохранено");
+            });
+        }   
     };
 };
