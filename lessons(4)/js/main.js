2eff7f6e90f38b527e8460c4c25bcb7b304a5d38
ASTRAone <vladwau827@gmail.com>
2020-08-02 14:47:57 +0300

    script for finding unique elements

--- lessons(4)/js/main.js
+++ lessons(4)/js/main.js
@@ -1,4 +1,4 @@
-const ARRAY = [
+const massObj = [
     {
         name: 'John',
         tel: '12345',
@@ -19,8 +19,58 @@ const ARRAY = [
         tel: '12345',
         age: '18'
     },
+    {
+        name: 'John',
+        tel: '12345',
+        age: '18'
+    },
+    {
+        name: 'Mikle',
+        tel: '12345',
+        age: '18'
+    },
+    {
+        name: 'Mikle',
+        tel: '12345',
+        age: '18'
+    },
 ];
 
-let unique = ARRAY.reduce((accumulator, elem) => accumulator.add(elem.name), new Set())
+let parseMassObj = (massObj) => {
+    let newArr = [];
 
-console.log(unique)
+    massObj.forEach(elem => {
+        let newElem = JSON.stringify(elem);
+
+        if (newArr.indexOf(newElem) === -1) {
+            newArr.push(newElem);
+        }
+    });
+
+    let newArrObj = [];
+    newArr.forEach(elem => {
+        newArrObj.push(JSON.parse(elem));
+    });
+
+    return newArrObj;
+}
+
+console.log(parseMassObj(massObj))
+
+
+
+
+
+let strFind = (str1, str2) => {
+    let count = 0;
+
+    for (let i = 0; i < str2.length; i++) {
+        if (str1.indexOf(str2[i]) !== -1) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(strFind("qwerty", "qwertyui"))
