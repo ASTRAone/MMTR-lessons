d977f3d3fc03499981ff60564c0a5d24f04dd20f
ASTRAone <vladwau827@gmail.com>
2020-08-05 22:35:15 +0300

    update wood

--- lessons(3)/wood/js/main.js
+++ lessons(3)/wood/js/main.js
@@ -1,31 +1,81 @@
-function Menu(options) {
-    let elem = options.elem;
+// function Menu(options) {
+//     let elem = options.elem;
     
-    // Открыть / закрыть основное меню
-    elem.onclick = function(event) {
-        if (event.target.closest(".menu__span")) {
-            let subMenuVisible = elem.querySelector(".sub-menu-b");
-            let subMenuBottom = elem.querySelector(".sub-menu-bottom");
+    
+//     elem.onclick = function(event) {
+//         // Открыть / закрыть основное меню
+//         if (event.target.closest(".top-menu__title")) {
+//             let subMenu = elem.querySelector(".sub-menu");
 
-            if (subMenuVisible.style.opacity === "1") {
-                subMenuVisible.style.opacity = 0;
-            } else {
-                subMenuVisible.style.opacity = 1;
-            }
-                
-            if (subMenuBottom.style.opacity === "1") {
-                        subMenuBottom.style.opacity = 0;
-            }
-        }
-        // Открыть / закрыть второстепенное меню
-        if (event.target.closest(".sub-menu__item_cl")) {
-            let subMenuBottom = elem.querySelector(".sub-menu-bottom");
+//             if (subMenu.style.display === "block") {
+//                 subMenu.style.display = "none";
+//             } else {
+//                 subMenu.style.display = "block"
+//             }
 
-            if (subMenuBottom.style.opacity === "1") {
-                subMenuBottom.style.opacity = 0;
-            } else {
-                subMenuBottom.style.opacity = 1;
-            }
-        }
-    };
-};
+//             // subMenu.style.display = "block";
+//         }
+//         // Открыть / закрыть меню третьего уровня
+//         if (event.target.closest(".sub-menu__item_b")) {
+//             let subMenuTwo = elem.querySelector(".sub-menu-two");
+//             subMenuTwo.style.display = "block"
+//         }
+//         // Открыть / закрыть меню четвертого уровня
+//         if (event.target.closest(".sub-menu-two__item_b")) {
+//             let subMenuThree = elem.querySelector(".sub-menu-three");
+//             subMenuThree.style.display = "block"
+//         }
+//     };
+// };
+
+// Меню второго уровня
+let subMenuSpan = document.querySelector(".sub-menu__span");
+
+subMenuSpan.addEventListener("click", () => {
+    let subMenu = document.querySelector(".sub-menu");
+    
+    if (subMenu.style.display === "block") {
+        subMenu.style.display = "none";
+    } else {
+        subMenu.style.display = "block"
+    }
+});
+
+// Меню третьего уровня
+let subMenuSpanTwo = document.querySelector(".sub-menu__span_two");
+
+subMenuSpanTwo.addEventListener("click", () => {
+    let subMenuTwo = document.querySelector(".sub-menu-two");
+    
+    if (subMenuTwo.style.display === "block") {
+        subMenuTwo.style.display = "none";
+    } else {
+        subMenuTwo.style.display = "block"
+    }
+});
+
+// Меню четвертого уровня
+let subMenuSpanThree = document.querySelector(".sub-menu__span_three");
+
+subMenuSpanThree.addEventListener("click", () => {
+    let subMenuThree = document.querySelector(".sub-menu-three");
+    
+    if (subMenuThree.style.display === "block") {
+        subMenuThree.style.display = "none";
+    } else {
+        subMenuThree.style.display = "block"
+    }
+});
+
+// Открыть второе меню второго уровня
+let subMenuSpanTwoB = document.querySelector(".sub-menu__span_two-b");
+
+subMenuSpanTwoB.addEventListener("click", () => {
+    let subMenuTwoB = document.querySelector(".sub-menu-two_b");
+
+    if (subMenuTwoB.style.display === "block") {
+        subMenuTwoB.style.display = "none";
+    } else {
+        subMenuTwoB.style.display = "block";
+    }
+});
