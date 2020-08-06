987101e4c158fa8c1b49aac2c80af7c4c0619bfd
ASTRAone <vladwau827@gmail.com>
2020-08-06 20:50:46 +0300

    update code wood

--- lessons(3)/wood/js/main.js
+++ lessons(3)/wood/js/main.js
@@ -1,33 +1,3 @@
-// function Menu(options) {
-//     let elem = options.elem;
-    
-    
-//     elem.onclick = function(event) {
-//         // Открыть / закрыть основное меню
-//         if (event.target.closest(".top-menu__title")) {
-//             let subMenu = elem.querySelector(".sub-menu");
-
-//             if (subMenu.style.display === "block") {
-//                 subMenu.style.display = "none";
-//             } else {
-//                 subMenu.style.display = "block"
-//             }
-
-//             // subMenu.style.display = "block";
-//         }
-//         // Открыть / закрыть меню третьего уровня
-//         if (event.target.closest(".sub-menu__item_b")) {
-//             let subMenuTwo = elem.querySelector(".sub-menu-two");
-//             subMenuTwo.style.display = "block"
-//         }
-//         // Открыть / закрыть меню четвертого уровня
-//         if (event.target.closest(".sub-menu-two__item_b")) {
-//             let subMenuThree = elem.querySelector(".sub-menu-three");
-//             subMenuThree.style.display = "block"
-//         }
-//     };
-// };
-
 // Меню второго уровня
 let subMenuSpan = document.querySelector(".sub-menu__span");
 
@@ -64,18 +34,5 @@ subMenuSpanThree.addEventListener("click", () => {
         subMenuThree.style.display = "none";
     } else {
         subMenuThree.style.display = "block"
-    }
-});
-
-// Открыть второе меню второго уровня
-let subMenuSpanTwoB = document.querySelector(".sub-menu__span_two-b");
-
-subMenuSpanTwoB.addEventListener("click", () => {
-    let subMenuTwoB = document.querySelector(".sub-menu-two_b");
-
-    if (subMenuTwoB.style.display === "block") {
-        subMenuTwoB.style.display = "none";
-    } else {
-        subMenuTwoB.style.display = "block";
     }
 });
