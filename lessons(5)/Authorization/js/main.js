2d1693b932fec7f3570c0e443d435982df1b1592
ASTRAone <vladwau827@gmail.com>
2020-08-09 22:21:23 +0300

    create form

---
+++ lessons(5)/Authorization/js/main.js
@@ -0,0 +1,37 @@
+
+// Проверка на пароль (создал функцией, чтобы при необходимости добавить функционал)
+validatePass = (pass) => {
+    return pass.length < 3 ? false : true;
+};
+
+// Проверка email
+validateEmail = (email) => {
+    if (email.length === 0) return false;
+
+    let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(String(email).toLowerCase());
+};
+
+// Отправка данных
+let btnForm = document.querySelector(".form__btn");
+
+btnForm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let formSubmit = document.querySelector(".form");
+    let mail = document.querySelector(".form__input_mail").value;
+    let pass = document.querySelector(".form__input_pass").value;
+    let checkbox = document.querySelector(".form__input-ch").checked === true ? true : false;
+
+    if (validateEmail(mail) && validatePass(pass)) {
+        formSubmit.submit();
+        formSubmit.reset();
+
+        let mistake = document.querySelector(".form__mistake");
+        mistake.style.opacity = 0;
+    } 
+    else {
+        let mistake = document.querySelector(".form__mistake");
+        mistake.style.opacity = 1;
+    }
+});
