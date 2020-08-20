3449bf50c976146b26efd100bc2e4fae547ce8f3
ASTRAone <vladwau827@gmail.com>
2020-08-20 10:13:59 +0300

    reactagl

--- lessons(6)/js/main.js
+++ lessons(6)/js/main.js
@@ -1,5 +1,7 @@
 creacteRect = (x, y, wd, hg) => {
     let container = document.querySelector(".container-rectangles");
+    // let container = document.body;
+
 
         let rect = document.createElement("div");
         rect.classList.add("rect");
@@ -9,14 +11,27 @@ creacteRect = (x, y, wd, hg) => {
         rect.style.top = y + "px";
     
         // Размеры
-        rect.style.width = wd;
-        rect.style.height = hg;
+        rect.style.width = wd + "px";
+        rect.style.height = hg + "px";
     
         container.appendChild(rect);
 
         for (let i = 0; i < 10; i++) {
-            
+            let rectGrid = document.createElement("div");
+            rectGrid.classList.add("rect");
+
+            // Позиция
+            // rectGrid.style.left = x + wd + "px" ;
+            rectGrid.style.top = y + "px";
+
+            container.appendChild(rectGrid);
+
+            // Позиция
+            rectGrid.style.left = x + (wd * 2) + "px" ;
+            rectGrid.style.top = y + "px";
         }
+
+        
 };
 
-creacteRect(0, 0, 40, 20)
+creacteRect(175, 80, 40, 20)
