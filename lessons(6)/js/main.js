783c4ff4856f80aaff88d009b93a2e6a3b141e58
ASTRAone <vladwau827@gmail.com>
2020-08-20 10:13:59 +0300

    reactagl

--- lessons(6)/js/main.js
+++ lessons(6)/js/main.js
@@ -1,22 +1,58 @@
-creacteRect = (x, y, wd, hg) => {
-    let container = document.querySelector(".container-rectangles");
-
-        let rect = document.createElement("div");
-        rect.classList.add("rect");
+// Создание центрального прямоугольника
+creacteRectCenter = (x, y, wd, hg) => {
+    let container = document.body;
     
-        // Позиция
-        rect.style.left = x + "px";
-        rect.style.top = y + "px";
+    let rect = document.createElement("div");
+    rect.classList.add("rect");
     
-        // Размеры
-        rect.style.width = wd;
-        rect.style.height = hg;
+    // Позиция
+    rect.style.left = x + "px";
+    rect.style.top = y + "px";
     
-        container.appendChild(rect);
-
-        for (let i = 0; i < 10; i++) {
-            
-        }
+    // Размеры
+    rect.style.width = wd + "px";
+    rect.style.height = hg + "px";
+    
+    container.appendChild(rect);
 };
 
-creacteRect(0, 0, 40, 20)
+// Заполнение сетки
+createRects = () => {
+    let container = document.body;
+    let rect = document.querySelector(".rect");
+
+    let x = parseInt(rect.style.left);
+    let y = parseInt(rect.style.top);
+    let wd = parseInt(rect.style.width);
+    let hg = parseInt(rect.style.height);
+
+    // for (let i = 1; i < 110; i++) {
+    //     let rectGrid = document.createElement("div");
+    //     rectGrid.classList.add("rect-grid");
+    
+    //     rectGrid.style.left = x + wd * i + "px";
+    //     rectGrid.style.top = y + "px";
+
+    //     if (rectGrid.style.left > 1061 + "px") {
+    //         x = 0
+    //         let newY = y + hg * i + "px";
+
+    //         rectGrid.style.left = x + wd + "px";
+    //         rectGrid.style.top = newY + "px";
+    //     }
+    
+    //     container.appendChild(rectGrid);
+    // }
+
+    let rectGrid = document.createElement("div");
+    rectGrid.classList.add("rect-grid");
+
+    rectGrid.style.left = x + wd + "px";
+    rectGrid.style.top = y + "px";
+
+    container.appendChild(rectGrid);
+};
+
+creacteRectCenter(660, 300, 40, 20);
+
+createRects();
