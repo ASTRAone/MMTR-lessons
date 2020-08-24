e04cbefca86cab38cd85ca341ad9b5015ca2e3e7
ASTRAone <vladwau827@gmail.com>
2020-08-24 20:07:00 +0300

    update rect

--- lessons(6)/js/main.js
+++ lessons(6)/js/main.js
@@ -16,50 +16,148 @@ creacteRectCenter = (x, y, wd, hg) => {
     container.appendChild(rect);
 };
 
+createLineGrid = (x, y, wd, hg, lenthLine, LENGTH_TOP, d_top, d_bottom) => {
+    for (let i = 1; i < lenthLine; i++) {
+
+        let container = document.body;
+        let rectGrid = document.createElement("div");
+            rectGrid.classList.add("rect-grid");
+
+        let left;
+        let top;
+
+        // Определение знака
+        switch(d_top) {
+            case "+": left = x + wd * i;
+            break;
+
+            case "-": left = x - wd * i;
+            break;
+
+            default: return;
+        }
+
+        switch(d_bottom) {
+            case "+": top = y + hg * LENGTH_TOP;
+            break;
+
+            case "-": top = y - hg * LENGTH_TOP;
+            break;
+
+            default: return;
+        }
+            
+        // Координаты фигуры
+        rectGrid.style.left = left + "px";
+        rectGrid.style.top = top + "px";
+        
+        container.appendChild(rectGrid);
+    }
+};
+
 // Заполнение сетки
 createRects = () => {
-
-    // Сделать const
-
 
     let container = document.body;
     let rect = document.querySelector(".rect");
 
-    let x = parseInt(rect.style.left);
-    let y = parseInt(rect.style.top);
-    let wd = parseInt(rect.style.width);
-    let hg = parseInt(rect.style.height);
+    const x = parseInt(rect.style.left);
+    const y = parseInt(rect.style.top);
+    const wd = parseInt(rect.style.width);
+    const hg = parseInt(rect.style.height);
+    const LENGTH = 5;
+    let LENGTH_TOP = 1;
+    
 
-    for (let i = 1; i < 5; i++) {
+    // Вправо
+    for (let i = 1; i < LENGTH; i++) {
         let rectGrid = document.createElement("div");
         rectGrid.classList.add("rect-grid");
+
+        let left = x + wd * i;
+        let top = y;
     
-        rectGrid.style.left = x + wd * i + "px";
-        rectGrid.style.top = y + "px";
+        rectGrid.style.left = left + "px";
+        rectGrid.style.top = top + "px";
+    
+        container.appendChild(rectGrid);
+
+        // Вправо и вниз
+        createLineGrid(x, y, wd, hg, LENGTH, LENGTH_TOP, "+", "+")
+        LENGTH_TOP++;
+    }
+
+    LENGTH_TOP = 1
+
+    // Вверх
+    for (let i = 1; i < LENGTH; i++) {
+        let rectGrid = document.createElement("div");
+        rectGrid.classList.add("rect-grid");
+
+        let left = x;
+        let top = y - hg * i;
+    
+        rectGrid.style.left = left + "px";
+        rectGrid.style.top = top + "px";
+    
+        container.appendChild(rectGrid);
+        
+        // Вверх и вправо
+        createLineGrid(x, y, wd, hg, LENGTH, LENGTH_TOP, "+", "-")
+
+        // Вверх и влево
+        createLineGrid(x, y, wd, hg, LENGTH, LENGTH_TOP, "-", "-")
+
+        LENGTH_TOP++;
+    }
+
+    LENGTH_TOP = 1;
+
+    // Вниз
+    for (let i = 1; i < LENGTH; i++) {
+        let rectGrid = document.createElement("div");
+        rectGrid.classList.add("rect-grid");
+
+        let left = x;
+        let top = y + hg * i;
+    
+        rectGrid.style.left = left + "px";
+        rectGrid.style.top = top + "px";
     
         container.appendChild(rectGrid);
     }
 
-    for (let i = 1; i < 5; i++) {
+    // Влево
+    for (let i = 1; i < LENGTH; i++) {
         let rectGrid = document.createElement("div");
         rectGrid.classList.add("rect-grid");
+
+        let left = x - wd * i;
+        let top = y;
     
-        rectGrid.style.left = x + "px";
-        rectGrid.style.top = y + hg * i + "px";
+        rectGrid.style.left = left + "px";
+        rectGrid.style.top = top + "px";
     
         container.appendChild(rectGrid);
+
+        // Влево и вниз
+        createLineGrid(x, y, wd, hg, LENGTH, LENGTH_TOP, "-", "+")
+
+        LENGTH_TOP++;
     }
 
-    // let rectGrid = document.createElement("div");
-    // rectGrid.classList.add("rect-grid");
-
-    // rectGrid.style.left = x + wd + "px";
-    // rectGrid.style.top = y + "px";
-
-    // container.appendChild(rectGrid);
-    
+    LENGTH_TOP = 1;
 };
 
 creacteRectCenter(660, 300, 40, 20);
 
-createRects();
+// Создать сетку
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    createRects();
+
+    btn.style.display = "none"
+});
+
+
